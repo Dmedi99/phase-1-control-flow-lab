c6fe58c0a05659ce2d39feb32a1f9ac5af48e64b
David <davidmedina0907@gmail.com>
2023-03-31 22:40:05 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400){
+    result = `This one is on me!`
+  } else {
+    result = `That will be twenty bucks.`
+    if (someValue > 2000){
+      result = `I will gladly take your thirty bucks.`
+    }
+    if (someValue > 2500){
+      result = `No can do.`
+    }
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let response = city == `NYC` ? `Ok, sounds good.` : 'No go.'
+return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+switch(tipAmount){
+  case `generous`:
+    return(`Thank you so much.`)
+  case `not as generous`:
+    return(`Thank you.`)
+  default:
+    return(`Bye.`)
+}
 }
